7e225c7c1cf7dfbdf4a5a551ae7805c5a5f9b985
Vipransh Tandon <vipransh.td@gmail.com>
2026-06-10 16:54:35 +0530

    Implemented login API with JWT authentication

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,3 +1,4 @@
+const jwt = require("jsonwebtoken");
 const pool = require("../config/db");
 const bcrypt = require("bcrypt");
 
@@ -31,6 +32,59 @@ const registerUser = async (req, res) => {
   }
 };
 
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const result = await pool.query(
+      "SELECT * FROM users WHERE email = $1",
+      [email]
+    );
+
+    if (result.rows.length === 0) {
+      return res.status(404).json({
+        message: "User not found"
+      });
+    }
+
+    const user = result.rows[0];
+
+    const isMatch = await bcrypt.compare(
+      password,
+      user.password_hash
+    );
+
+    if (!isMatch) {
+      return res.status(401).json({
+        message: "Invalid password"
+      });
+    }
+
+    const token = jwt.sign(
+      {
+        userId: user.user_id,
+        email: user.email
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "7d"
+      }
+    );
+
+    res.status(200).json({
+      message: "Login successful",
+      token
+    });
+
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      message: "Login failed"
+    });
+  }
+};
 module.exports = {
   registerUser,
+  loginUser,
 };
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 const {
   registerUser,
+  loginUser
 } = require("../controllers/authController");
 
 router.post("/register", registerUser);
+router.post("/login", loginUser);
 
 module.exports = router;
